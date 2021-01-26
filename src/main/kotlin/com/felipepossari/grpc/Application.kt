c8f1bc0ed3f4f6f2f225faeff6a03e76dbb1d5cb
Felipe Possari <fcpfelipe@hotmail.com>
2021-01-26 08:38:02 -0300

    Code updated to start the GRPC server manually

--- src/main/kotlin/com/felipepossari/grpc/Application.kt
+++ src/main/kotlin/com/felipepossari/grpc/Application.kt
@@ -1,10 +1,29 @@
 package com.felipepossari.grpc
 
-import io.micronaut.runtime.Micronaut.*
-fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("com.felipepossari.grpc")
-		.start()
-}
+import io.grpc.Server
+import io.grpc.ServerBuilder
 
+//fun main(args: Array<String>) {
+//	build()
+//	    .args(*args)
+//		.packages("com.felipepossari.grpc")
+//		.start()
+//}
+
+
+fun main() {
+    println("Starting server")
+    val server: Server = ServerBuilder.forPort(50051)
+            .build()
+
+    server.start()
+
+    Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("Shutting server")
+                server.shutdown()
+            }
+    )
+
+    server.awaitTermination()
+}
