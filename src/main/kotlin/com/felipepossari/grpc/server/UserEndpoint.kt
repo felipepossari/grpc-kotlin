8bab44a5d6d22300ad89db1ea1af1456b4719198
Felipe Possari <fcpfelipe@hotmail.com>
2021-01-27 08:03:28 -0300

    User endpoint updated to handle the sendPush method

--- src/main/kotlin/com/felipepossari/grpc/server/UserEndpoint.kt
+++ src/main/kotlin/com/felipepossari/grpc/server/UserEndpoint.kt
@@ -1,18 +1,44 @@
 package com.felipepossari.grpc.server
 
+import com.felipepossari.grpc.PushRequest
+import com.felipepossari.grpc.PushResponse
+import com.felipepossari.grpc.Status
 import com.felipepossari.grpc.UserCreateRequest
 import com.felipepossari.grpc.UserCreateResponse
 import com.felipepossari.grpc.UserServiceGrpcKt
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.util.*
 
 class UserEndpoint : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
 
     override suspend fun create(request: UserCreateRequest): UserCreateResponse {
 
-        return UserCreateResponse.newBuilder()
-                .setCountry(request.country)
-                .setEmail(request.email)
-                .setName(request.name)
-                .setId(1L)
-                .build()
+        return UserCreateResponse.newBuilder().apply {
+            email = request.email;
+            name = request.name;
+            country = request.country
+        }.build()
     }
+
+    override fun sendPush(request: PushRequest): Flow<PushResponse> = flow {
+        val transaction = UUID.randomUUID().toString()
+        for (i in 1..5) {
+            kotlinx.coroutines.delay(1000)
+            emit(buildPendingPushResponse(transaction))
+        }
+        emit(buildApprovedPushResponse(transaction))
+    }
+
+    private fun buildApprovedPushResponse(transaction: String) =
+            buildPushResponse(transaction, Status.APPROVED)
+
+    private fun buildPendingPushResponse(transaction: String) =
+            buildPushResponse(transaction, Status.PENDING)
+
+    private fun buildPushResponse(transaction: String, statusPush: Status) =
+            PushResponse.newBuilder().apply {
+                transactionId = transaction;
+                status = statusPush
+            }.build()
 }
