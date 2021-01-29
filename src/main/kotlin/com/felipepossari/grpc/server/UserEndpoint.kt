e654d8dfa01d3767b1e0551643f2298eb98bb213
Felipe Possari <fcpfelipe@hotmail.com>
2021-01-29 08:30:42 -0300

    Exceptions handled in the server

--- src/main/kotlin/com/felipepossari/grpc/server/UserEndpoint.kt
+++ src/main/kotlin/com/felipepossari/grpc/server/UserEndpoint.kt
@@ -9,6 +9,7 @@ import com.felipepossari.grpc.UserBulkCreateResponse
 import com.felipepossari.grpc.UserCreateRequest
 import com.felipepossari.grpc.UserCreateResponse
 import com.felipepossari.grpc.UserServiceGrpcKt
+import io.grpc.StatusException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
@@ -21,10 +22,10 @@ class UserEndpoint : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
 
     private val random = Random(123456)
 
-    override suspend fun create(request: UserCreateRequest): UserCreateResponse = kotlin.runCatching {
+    override suspend fun create(request: UserCreateRequest): UserCreateResponse = runCatching {
 
-        if(request.email.isEmpty()){
-            throw Exception("Email null or empty")
+        if (request.email.isEmpty()) {
+            throw IllegalArgumentException("Email null or empty")
         }
 
         return UserCreateResponse.newBuilder().apply {
@@ -32,30 +33,27 @@ class UserEndpoint : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
             name = request.name;
             country = request.country
         }.build()
-    }.onFailure { e ->
-        throw io.grpc.Status.INVALID_ARGUMENT
-                .withCause(e.cause)
-                .withDescription(e.message)
-                .asRuntimeException()
-    }.getOrThrow()
+    }.onFailure { e -> throw e.toStatusException() }.getOrThrow()
 
-    override fun sendPush(request: PushRequest): Flow<PushResponse> = flow {
-        val transaction = UUID.randomUUID().toString()
-        val randomPushApproval = random.nextInt(1..10)
-        for (i in 1..10) {
-            kotlinx.coroutines.delay(500)
-            if (i == randomPushApproval) {
-                println("Push response: APPROVED")
-                emit(buildApprovedPushResponse(transaction))
-                break;
-            } else {
-                println("Push response: PENDING")
-                emit(buildPendingPushResponse(transaction))
+    override fun sendPush(request: PushRequest): Flow<PushResponse> = runCatching {
+        flow {
+            val transaction = UUID.randomUUID().toString()
+            val randomPushApproval = random.nextInt(1..10)
+            for (i in 1..10) {
+                kotlinx.coroutines.delay(500)
+                if (i == randomPushApproval) {
+                    println("Push response: APPROVED")
+                    emit(buildApprovedPushResponse(transaction))
+                    break;
+                } else {
+                    println("Push response: PENDING")
+                    emit(buildPendingPushResponse(transaction))
+                }
             }
         }
-    }
+    }.onFailure { e -> throw e.toStatusException() }.getOrThrow()
 
-    override suspend fun bulkCreate(requests: Flow<UserCreateRequest>): UserBulkCreateResponse {
+    override suspend fun bulkCreate(requests: Flow<UserCreateRequest>): UserBulkCreateResponse = runCatching {
         println("User bulk create request received")
         var usersCreated = 0L
 
@@ -67,17 +65,19 @@ class UserEndpoint : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
         }
         println("Users created: $usersCreated")
         return UserBulkCreateResponse.newBuilder().apply { count = usersCreated }.build()
-    }
+    }.onFailure { e -> throw e.toStatusException() }.getOrThrow()
 
-    override fun sendAction(requests: Flow<UserActionRequest>): Flow<UserActionResponse> = flow {
-        requests.collect {
-            println("Action received. Id: ${it.actionId}")
-            for (i in 1..it.repeatTimes) {
-                emit(buildUserActionResponse(it.actionId, it.repeatTimes))
-                delay(250)
+    override fun sendAction(requests: Flow<UserActionRequest>): Flow<UserActionResponse> = runCatching {
+        flow {
+            requests.collect {
+                println("Action received. Id: ${it.actionId}")
+                for (i in 1..it.repeatTimes) {
+                    emit(buildUserActionResponse(it.actionId, it.repeatTimes))
+                    delay(250)
+                }
             }
         }
-    }
+    }.onFailure { e -> throw e.toStatusException() }.getOrThrow()
 
     private fun buildUserActionResponse(requestActionId: Long, repeatTimes: Int) =
             UserActionResponse.newBuilder().apply {
@@ -96,4 +96,16 @@ class UserEndpoint : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
                 transactionId = transaction;
                 status = statusPush
             }.build()
+
+    private fun Throwable.toStatusException(): StatusException =
+            when (this) {
+                is IllegalArgumentException -> io.grpc.Status.INVALID_ARGUMENT
+                        .withCause(this.cause)
+                        .withDescription(this.message)
+                        .asException()
+                else -> io.grpc.Status.INTERNAL
+                        .withCause(this.cause)
+                        .withDescription(this.message)
+                        .asException()
+            }
 }
